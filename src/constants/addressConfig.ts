6d46106d5f6dabf9020384f7aedd0189c6bee8ee
luanxu-mxc <luanxu@mxc.org>
2024-04-11 22:09:18 +0800

    chore: update mep1002 address

--- src/constants/addressConfig.ts
+++ src/constants/addressConfig.ts
@@ -4,7 +4,7 @@ const chain_id: string = process.env.NEXT_PUBLIC_NETWORK_CHAINID || "18686"
 
 const contracts: any = {
     5167004: {
-        MEP1002: "0x181eC6Bc420FAEce0228c91936938F438F2a33df",
+        MEP1002: "0x1964F08f56b79051fB3AE9a2C4d8D92A059b1237",
     },
     18686: {
         MEP1002: "0x068234de9429FaeF2585A6eD9A52695cDa78aFE1",
